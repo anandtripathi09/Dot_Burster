47a5f29a749cd0ac769f0e051044fa757d0c2742
Anand Tripathi <anand.t9903@gmail.com>
2025-06-29 13:18:48 +0530

    Update GamePage.tsx

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSocket } from '../contexts/SocketContext';
