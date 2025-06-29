9973f99625a86fbe0578aaf85e9d7714e06d193f
Anand Tripathi <anand.t9903@gmail.com>
2025-06-29 16:15:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://dot-burster-6.onrender.com"],
+    origin: ["http://localhost:5173", "https://dot-burster-1.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true
   }
