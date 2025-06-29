8029531054436518ec90bc2ff403bd288acd3dfb
Anand Tripathi <anand.t9903@gmail.com>
2025-06-29 13:18:35 +0530

    Update DemoGame.tsx

--- src/pages/DemoGame.tsx
+++ src/pages/DemoGame.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
